5042375ec32d42a66e09731828a25507a87a43a0
olganedelcu <olganedelcuam@gmail.com>
2020-06-06 20:52:43 +0200

    bubble colors

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ window.addEventListener('load', () => {
 
     const sounds = document.querySelectorAll(".sound");
     const pads = document.querySelectorAll(".pads div");
+    const visual = document.querySelector(".visual");
+    const colors = [
+        "#60d394",
+        "#d36060",
+        "#c060d3",
+        "#d3d160",
+        "#606bd3",
+        "#60c2d3"
+      ];
+    
 
     //Sound
     pads.forEach((pad, index) => {
