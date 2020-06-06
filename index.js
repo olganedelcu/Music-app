3c73ac4c75ef2038c1f8042aa63a145eee505d77
olganedelcu <olganedelcuam@gmail.com>
2020-06-06 20:45:35 +0200

    pads for Each

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 window.addEventListener('load', () => {
+
     const sounds = document.querySelectorAll(".sound");
+    const pads = document.querySelectorAll(".pads div");
+
+    //Sound
+    pads.forEach(pad =>{
+        pad.addEventListener("click", function() {
+            sounds[4]
+        });
+    });
 });
